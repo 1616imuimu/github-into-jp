53c0effd51eaf90ee57e6fe87ac22f1941497c95
1616imuimu <yagasaki.kei@gmail>
2022-02-20 00:05:09 +0900

    [共通][ヘッダー] 新規作成メニューの翻訳を実装

--- tl_header.js
+++ tl_header.js
@@ -13,6 +13,18 @@ function tl_marketplace(el){
 function tl_explore(el){
     el.textContent = el.textContent.replace(/explore/i, "エクスプローラー");
 }
+function tl_new_repository(el){
+    el.textContent = el.textContent.replace(/new repository/i, "リポジトリを作る");
+}
+function tl_import_repository(el){
+    el.textContent = el.textContent.replace(/import repository/i, "リポジトリを読み込む");
+}
+function tl_new_organization(el){
+    el.textContent = el.textContent.replace(/new organization/i, "組織を作る");
+}
+function fix_dropdown_menu_width(el){
+    el.style.width = "170px";
+}
 
 try {
     let body = document.body
@@ -27,6 +39,13 @@ try {
         tl_issue(header_nav.querySelector("a[href='/issues']"))
         tl_marketplace(header_nav.querySelector("a[href='/marketplace']"))
         tl_explore(header_nav.querySelector("a[href='/explore']"))
+        
+        fix_dropdown_menu_width(header.querySelector("details-menu"))
+        tl_new_repository(header.querySelector("details-menu a[href='/new']"))
+        tl_import_repository(header.querySelector("details-menu a[href='/new/import']"))
+        // tl_new_gist(header.querySelector("details-menu a[href='https://gist.github.com/']"))
+        tl_new_organization(header.querySelector("details-menu a[href='/organizations/new']"))
+        // tl_new_project(header.querySelector("details-menu a[href='/users/']"))
     }
 
 } catch (error) {
